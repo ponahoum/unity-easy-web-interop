a6aecc2a58f4f9115cdf30b5be183e89120527cd
Pierre-Olivier <pierre-olivier@prevu3d.com>
2024-12-13 14:10:08 -0500

    Protect against double setup

--- Packages/org.nahoum.easy-web-interop/Runtime/EasyWebInterop.cs
+++ Packages/org.nahoum.easy-web-interop/Runtime/EasyWebInterop.cs
@@ -7,13 +7,19 @@ namespace Nahoum.UnityJSInterop
 {
     public static class UnityJSInterop
     {
+        // Protection against double setup
+        static bool isSetup = false;
+
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void Setup()
         {
-            #if !UNITY_EDITOR && UNITY_WEBGL
+#if !UNITY_EDITOR && UNITY_WEBGL
+            if(isSetup)
+            throw new System.Exception("UnityJSInterop is already setup. You should only call this method once.");
             InternalInteropSetup.Setup();
             AutoRegister.Setup();
-            #endif
+            isSetup = true;
+#endif
         }
     }
 }
