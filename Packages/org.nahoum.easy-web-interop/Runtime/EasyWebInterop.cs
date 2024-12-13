9871da65a85e306590c95613e8790c6a25403138
Pierre-Olivier <pierre-olivier@prevu3d.com>
2024-12-13 14:19:40 -0500

    Fix interop

--- Packages/org.nahoum.easy-web-interop/Runtime/EasyWebInterop.cs
+++ Packages/org.nahoum.easy-web-interop/Runtime/EasyWebInterop.cs
@@ -7,19 +7,19 @@ namespace Nahoum.UnityJSInterop
 {
     public static class UnityJSInterop
     {
+#if !UNITY_EDITOR && UNITY_WEBGL
         // Protection against double setup
         static bool isSetup = false;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void Setup()
         {
-#if !UNITY_EDITOR && UNITY_WEBGL
             if(isSetup)
             throw new System.Exception("UnityJSInterop is already setup. You should only call this method once.");
             InternalInteropSetup.Setup();
             AutoRegister.Setup();
             isSetup = true;
-#endif
         }
+#endif
     }
 }
